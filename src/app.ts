8405895019a0ea308ec10be777a7e4e90f683e2c
Joui Bomfim <jouibomfim@gmail.com>
2024-10-17 11:06:12 -0300

    Alterações para funcionamento do backend

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+//import { DatabaseModel } from './model/DatabaseModel';
+import { server } from './server';
+
+const port: number = 3333;
+console.log(`Servidor rodando em http://localhost:${3333}`);
+
+ /*
+new DatabaseModel().testeConexao().then((resbd) => {
+    if(resbd) {
+        server.listen(3333, () => {
+            console.log(`Servidor rodando em http://localhost:${3333}`);
+        })
+    } else {
+        console.log('Não foi possível conectar ao banco de dados');
+    }
+});
+
+*/
